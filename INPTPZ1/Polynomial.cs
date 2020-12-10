52d640a355a9b6e791b4ef66ce4066d729d0fca0
barpi <st52947@student.upce.cz>
2020-12-10 09:38:13 +0100

    Refactor of Polynomial

--- INPTPZ1/Polynomial.cs
+++ INPTPZ1/Polynomial.cs
@@ -1,67 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace INPTPZ1
 {
     class Polynomial
     {
-        public List<ComplexNumber> Coe { get; set; }
+        public List<ComplexNumber> Coefficients { get; set; }
 
-        public Polynomial() => Coe = new List<ComplexNumber>();
+        public Polynomial() => Coefficients = new List<ComplexNumber>();
 
         public Polynomial Derive()
         {
-            Polynomial p = new Polynomial();
-            for (int i = 1; i < Coe.Count; i++)
+            Polynomial polynomial = new Polynomial();
+
+            for (int i = 1; i < Coefficients.Count; i++)
             {
-                p.Coe.Add(Coe[i].Multiply(new ComplexNumber() { Real = i }));
+                polynomial.Coefficients.Add(Coefficients[i].Multiply(new ComplexNumber() { Real = i }));
             }
 
-            return p;
+            return polynomial;
         }
 
-        public ComplexNumber Eval(ComplexNumber x)
+        public ComplexNumber Evaluate(ComplexNumber input)
         {
-            ComplexNumber s = ComplexNumber.Zero;
-            for (int i = 0; i < Coe.Count; i++)
+            ComplexNumber result = ComplexNumber.Zero;
+            for (int i = 0; i < Coefficients.Count; i++)
             {
-                ComplexNumber coef = Coe[i];
-                ComplexNumber bx = x;
+                ComplexNumber coefficient = Coefficients[i];
+                ComplexNumber evaluation = input;
                 int power = i;
 
                 if (i > 0)
                 {
                     for (int j = 0; j < power - 1; j++)
-                        bx = bx.Multiply(x);
+                        evaluation = evaluation.Multiply(input);
 
-                    coef = coef.Multiply(bx);
+                    coefficient = coefficient.Multiply(evaluation);
                 }
 
-                s = s.Add(coef);
+                result = result.Add(coefficient);
             }
 
-            return s;
+            return result;
         }
 
         public override string ToString()
         {
-            string s = "";
-            for (int i = 0; i < Coe.Count; i++)
+            string result = "";
+            for (int i = 0; i < Coefficients.Count; i++)
             {
-                s += Coe[i];
+                result += Coefficients[i];
                 if (i > 0)
                 {
                     for (int j = 0; j < i; j++)
                     {
-                        s += "x";
+                        result += "x";
                     }
                 }
-                s += " + ";
+                result += " + ";
             }
-            return s;
+            return result;
         }
     }
 }
